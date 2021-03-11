44a40d0d22458a736971ca04e3a1f53232d0b7e3
yixi66 <yixiou17@gmail.com>
2021-03-10 20:38:12 -0800

    close by the click

--- turtle_race.py
+++ turtle_race.py
@@ -1,31 +1,30 @@
 import turtle
 import time
 import random
-
+turtle.title('Turtle Race')
   
 # ask user 
 number_of_times = turtle.textinput("Question", "How many races would you like to see?") 
+
 if number_of_times:
-    print("The user want to see", number_of_times, "races")
-
+    print("The user wants to see", number_of_times, "races")
 else:
-    print("User don't want to answer.")
+    print("User doesn't want to answer.")
 
 
+# background color
 turtle.bgcolor("rosy brown")
 
 
-
-# cathy
+# cathy setting
 cathy = turtle.Turtle()
 cathy.shape('turtle')
 cathy.color("pink")
 
-# kathy
+# kathy setting
 kathy = turtle.Turtle()
 kathy.shape('turtle')
 kathy.color('green')
-
 
 
 # start line
@@ -53,22 +52,20 @@ turtle.goto(280,-330)
 turtle.write('finish',font=('Arial', 30, "normal"))
 
 
-
 # cathy back to the start line
 cathy.penup()
 cathy.goto(-320,150)
 
-
 # kathy back to the start line
 kathy.penup()
 kathy.goto(-320,-150)
+
 
 # write cathy distance
 turtle.color("linen")
 turtle.penup()
 turtle.goto(-290,300)
 turtle.write('Cathy distance: 0', font=('Arial', 20, "normal"))
-
 
 # write kathy distance
 turtle.penup()
@@ -80,15 +77,16 @@ turtle.pendown
 # ready
 time.sleep(1)
 
+
 # distance
-total_distance = 600
+total_distance = 620
 cathy_distance = 0
 kathy_distance = 0
 
-
+# turtles move
 speeds = [50, 70, 80, 90, 100, 120]
 from random import choice
-while(cathy_distance < 600 and kathy_distance < 600):
+while(cathy_distance < 620 and kathy_distance < 600):
     cathy_speed = random.choice(speeds)
     kathy_speed = random.choice(speeds)
 
@@ -102,7 +100,7 @@ while(cathy_distance < 600 and kathy_distance < 600):
     print('kathy_distance: ', kathy_distance)
 
 
-
+# write the result
 turtle.goto(-200,0)
 winner = cathy
 if cathy_distance >= 600 and kathy_distance < cathy_distance:
@@ -119,12 +117,12 @@ else:
     turtle.write('Kathy is winner', font=('Arial', 70, "normal"))
 
 
-# print(input("how many races would you like to see?  :  "))
+cathy.reset()
+kathy.reset()
 
 
-
-
-
+#close 
+turtle.exitonclick()
 
 
 turtle.mainloop()
